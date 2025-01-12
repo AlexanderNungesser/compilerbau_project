37bd8a7d3ae8eaad7e40a533fc5c2b652113efd3
marie <marie.juenke@gmx.de>
2025-01-12 10:11:17 +0100

    adjusting class and function

--- src/main/java/Class.java
+++ src/main/java/Class.java
@@ -1,4 +1,6 @@
 public class Class extends Symbol {
+  Scope classScope;
+
   public Class(String name) {
     super(name, null);
   }
@@ -6,4 +8,7 @@ public class Class extends Symbol {
   public Class(String name, String type) {
     super(name, type);
   }
+
+  public Scope getClassScope() {return this.classScope;}
+  public void setClassScope(Scope classScope) {this.classScope = classScope;}
 }
--- src/main/java/Function.java
+++ src/main/java/Function.java
@@ -1,5 +1,10 @@
 public class Function extends Symbol {
+  private int anzParams;
+
   public Function(String name, String type) {
     super(name, type);
   }
+
+  public int getAnzParams() {return this.anzParams;}
+  public void setAnzParams(int anzParams) {this.anzParams = anzParams;}
 }
