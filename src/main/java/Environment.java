ad9dd8acc6dedee4d0f701835e3e75739b422252
Alexander Nungesser <Alexander.Nungesser@wago.com>
2025-01-19 00:24:40 +0100

    spotlessApply

--- src/main/java/Environment.java
+++ src/main/java/Environment.java
@@ -2,33 +2,33 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Environment {
-    public Environment enclosingEnv;
-    public Map<String, Object> values = new HashMap<String, Object>();
+  public Environment enclosingEnv;
+  public Map<String, Object> values = new HashMap<String, Object>();
 
-    public Environment(Environment enclosing) {
-        this.enclosingEnv = enclosing;
-    }
+  public Environment(Environment enclosing) {
+    this.enclosingEnv = enclosing;
+  }
 
-    public void define(String name, Object value) {
-        this.values.put(name, value);
-    }
+  public void define(String name, Object value) {
+    this.values.put(name, value);
+  }
 
-    public void assign(String name, Object value) {
-        if(this.values.containsKey(name)){
-            this.values.put(name, value);
-        } else if (this.enclosingEnv != null) {
-            this.enclosingEnv.assign(name, value);
-        }else {
-            throw new RuntimeException("No value defined for " + name);
-        }
+  public void assign(String name, Object value) {
+    if (this.values.containsKey(name)) {
+      this.values.put(name, value);
+    } else if (this.enclosingEnv != null) {
+      this.enclosingEnv.assign(name, value);
+    } else {
+      throw new RuntimeException("No value defined for " + name);
     }
+  }
 
-    public Object get(String name) {
-        if (values.containsKey(name)) return values.get(name);
-        try {
-            return enclosingEnv.get(name);
-        } catch (Exception e) {
-            return null;
-        }
+  public Object get(String name) {
+    if (values.containsKey(name)) return values.get(name);
+    try {
+      return enclosingEnv.get(name);
+    } catch (Exception e) {
+      return null;
     }
+  }
 }
